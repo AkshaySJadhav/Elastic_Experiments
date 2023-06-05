7dea08e810de389c619955a4f588a8d9a6006fca
AkshaySJadhav <40785408+AkshaySJadhav@users.noreply.github.com>
2023-06-05 23:36:44 +0530

    Update certificate.sh

--- elk_scripts/certificate.sh
+++ elk_scripts/certificate.sh
@@ -6,24 +6,6 @@ openssl genrsa -passout pass:"$1" -des3 -out rootCA.key 2048
 # Generate Root PEM (rootCA.pem) with 1024 days validity.
 openssl req -passin pass:"$1" -subj "/C=US/ST=Random/L=Random/O=Global Security/OU=IT Department/CN=Local Certificate"  -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem
 
-# Add root cert as trusted cert
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux
-        yum -y install ca-certificates
-        update-ca-trust force-enable
-        cp rootCA.pem /etc/pki/ca-trust/source/anchors/
-        update-ca-trust
-        #meeting ES requirement
-        sysctl -w vm.max_map_count=262144
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain rootCA.pem
-else
-        # Unknown.
-        echo "Couldn't find desired Operating System. Exiting Now ......"
-        exit 1
-fi
-
 
 # Generate ES01 Cert
 openssl req -subj "/C=US/ST=Random/L=Random/O=Global Security/OU=IT Department/CN=localhost"  -new -sha256 -nodes -out es01.csr -newkey rsa:2048 -keyout es01.key
@@ -43,4 +25,4 @@ openssl x509 -req -passin pass:"$1" -in kib01.csr -CA rootCA.pem -CAkey rootCA.k
 
 cd ../elk
 
-VERSION="7.8.0" HOSTCERTSDIR="$PWD/../elkcerts" ESCERTSDIR="/usr/share/elasticsearch/config" KIBCERTSDIR="/usr/share/kibana/config" docker-compose up -d
+VERSION="8.8.0" HOSTCERTSDIR="$PWD/../elkcerts" ESCERTSDIR="/usr/share/elasticsearch/config" KIBCERTSDIR="/usr/share/kibana/config" docker-compose up -d
